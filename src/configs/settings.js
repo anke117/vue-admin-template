31f38fce51d78a3b0024c78ae3ca13435973a981
高阳 <2287795629@qq.com>
2020-12-16 20:10:50 +0800

    zen

--- src/configs/settings.js
+++ src/configs/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: 'Vue3-Admin-Template',
 
   /**
    * @type {boolean} true | false
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,5 +2,5 @@ import ElementPlus from 'element-plus'
 import '../theme/index.css'
 import '../element-variables.scss'
 export default (app) => {
-  app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+  app.use(ElementPlus, { zIndex: 3000 })
 }
