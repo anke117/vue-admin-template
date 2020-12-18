27cc9df9d428b03c20a659c506fc5b50acdfb4f7
高阳 <2287795629@qq.com>
2020-12-18 10:15:43 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import installElementPlus from './plugins/element.js'
-import installSvg from '@/assets/icons/index.js'
+import installElementPlus from './plugins/element.js' // element-ui
+import installSvg from '@/assets/icons' // svg图标
 import store from './store'
 import router from './router'
 import 'normalize.css/normalize.css'
 import '@/assets/styles/index.scss' // 全局样式
-// import '@/assets/icons' // svg图标
 import '@/configs/permission' // 路由层面权限的控制代码
 const app = createApp(App).use(router).use(store)
 installElementPlus(app)
